1f61a5210add1a7202bdd296a1a53dc4c768556a
imarcaos <marcaos.melo@gmail.com>
2023-05-14 13:16:11 +0100

    C195 - Add and clear paragraph input text, ex016

---
+++ Section06-JavaScript-Exercises/196-ex017-js-add_paragraph_no_repeat_phrases/app.js
@@ -0,0 +1,35 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 017
+Enunciado:
+    Ao clicar em "Adicionar", se o input tiver texto, adicionar o mesmo num
+    parágrafo por baixo do botão.
+
+    Não pode ser adicionada uma palavra ou frase repetida.
+---------------------------------------------------------------------------- */
+
+/*
+Date: 2023-05-14
+*/
+
+//----
+// info: input #text_post , button .btn, div text #posts
+
+let word_list = [];
+
+document.querySelector('.btn').addEventListener('click', () => {
+    let input = document.querySelector("#text_post");
+    if (input.value !== '') {
+        if (!word_list.includes(input.value)) {
+            let p = document.createElement('p');
+            p.textContent = input.value;
+            word_list.push(input.value);
+            document.querySelector("#posts").appendChild(p);
+            input.value = '';
+            input.focus();
+        } else {
+            input.value = '';
+            input.focus();
+        }
+    }
+})
